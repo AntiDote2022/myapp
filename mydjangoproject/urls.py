e2f8699f9bf8a4664d200e5a5159a0024023b231
AntiDote2022 <nematulloev.nur@gmail.com>
2023-01-10 17:49:20 +0900

    add file

--- mydjangoproject/urls.py
+++ mydjangoproject/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('account/', include('allauth.urls')),
     path('', index_page, name='home'),
     path('about/', about_page, name='about'),
     path('news/', include('news.urls')),
